fe9e869823963ecf194e5b6bef718ebb837a22bc
timojokinen <timojannejokinen@gmail.com>
2023-09-17 14:24:34 +0200

    initial commit

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { AuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: process.env.AUTH0_ISSUER!,
+    }),
+  ],
+};
